bcbf5fa48605d2b1042b6b34f11e616d9ca6a84e
Tugba Esat <tugbaesat007@gmail.com>
2023-04-17 14:32:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(messageArray, event) {
+  const thankYouMessage = [];
+  for (let i = 0; i < messageArray.length; i++) {
+    thankYouMessage.push(
+      `Thank you, ${messageArray[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return thankYouMessage;
+}
+function countDown(input) {
+    let i = input;
+    while (0 <= i) {
+        console.log(i--);
+    }
+}
